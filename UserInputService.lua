181dd285b4c58152f199eed47795d6f309b01e3e
Narrev <NarrevRBX@gmail.com>
2016-11-18 13:52:26 -0600

    Multi-thread Support
    Still runs first connection on the thread created by the InputBegan Connection, but runs subsequent connections on threads created by a BindableEvent.
    
    If this decreases efficiency for the first thread, this maybe shouldn't be implemented

--- UserInputService.lua
+++ UserInputService.lua
@@ -15,6 +15,7 @@ local Connect = InputService.InputBegan.Connect
 local Heartbeat = RunService.Heartbeat
 local Wait = Heartbeat.Wait
 local SetCore = StarterGui.SetCore
+local Destroy = game.Destroy
 local GetChildren = game.GetChildren
 
 local sub = string.sub
@@ -24,48 +25,86 @@ local remove = table.remove
 local newInstance = Instance.new
 local type, select, setmetatable, rawset, tostring, tick = type, select, setmetatable, rawset, tostring, tick
 
+local Fire, Disconnect do
+	local Bindable = newInstance("BindableEvent")
+	local Connection = Connect(Bindable.Event, function() end)
+	Fire, Disconnect = Bindable.Fire, Connection.Disconnect
+	Disconnect(Connection)
+	Destroy(Bindable)
+end
+
 -- Client
 local Player = Players.LocalPlayer
 local PlayerGui = Player:WaitForChild("PlayerGui")
 local PlayerMouse = Player:GetMouse()
 
 local function DisconnectConnector(self)
-	local func = self.func
-	local Connections = self.Connections
-	for a = 1, #Connections do
-		if Connections[a] == func then
-			remove(Connections, a)
-		end
-	end
+	self.Signal.Main = nil
 end
 
 local Disconnector = {Disconnect = DisconnectConnector}
 Disconnector.__index = Disconnector
 
 local function ConnectSignal(self, func)
-	if func then
-		self[#self + 1] = func
-		return setmetatable({Connections = self; func = func}, Disconnector)
+	-- Bind first function to this thread
+	-- Bind additional functions to bindable
+	assert(func, "Please connect a function to " .. self.KeyCode.Name)
+	if self.Main then
+		local Bindable = self.Bindable
+		if Bindable then
+			local Connections = self.Connections
+			local Connection = Connect(Bindable.Event, func)
+			if Connections then
+				Connections[#Connections + 1] = Connection
+			else
+				self.Connections = {Connection}
+			end
+			return Connection
+		else
+			local Bindable = newInstance("BindableEvent")
+			self.Bindable = Bindable
+			local Connection = Connect(Bindable.Event, func)
+			self.Connections = {Connection}
+			return Connection
+		end
+	else
+		self.Main = func
+		return setmetatable({Signal = self}, Disconnector)
 	end
 end
 
 local function DisconnectSignal(self)
-	for a = 1, #self do
-		self[a] = nil
+	self.Main = nil
+	local Connections = self.Connections
+	if Connections then
+		for a = 1, #Connections do
+			Disconnect(Connections[a])
+		end
+		self.Connections = nil
 	end
 end
 
 local function WaitSignal(self)
-	local Connection
-	Connection = ConnectSignal(self, function()
-		Connection = DisconnectConnector(Connection)
-	end)
-	repeat until not Connection or not Wait(Heartbeat)
+	local Bindable = self.Bindable
+	if Bindable then
+		Wait(Bindable.Event)
+	else
+		Bindable = newInstance("BindableEvent")
+		self.Bindable = Bindable
+		Wait(Bindable.Event)
+		if not self.Connections then
+			self.Bindable = Destroy(Bindable)
+		end
+	end
 end
 
-local function FireSignal(self, ...)
-	for a = 1, #self do
-		self[a](...)
+local function FireSignal(self)
+	local Main, Bindable = self.Main, self.Bindable
+	if Bindable then
+		Fire(Bindable)
+	end
+	if Main then
+		Main()
 	end
 end
 
