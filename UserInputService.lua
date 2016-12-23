b2e12a667f1fd462877b3e4c7dbfdcda3713c628
Narrev <NarrevRBX@gmail.com>
2016-12-23 01:57:09 -0600

    Update UserInputService.lua

--- UserInputService.lua
+++ UserInputService.lua
@@ -48,7 +48,7 @@ Disconnector.__index = Disconnector
 local function ConnectSignal(self, func)
 	-- Bind first function to this thread
 	-- Bind additional functions to bindable
-	assert(func, "Please connect a function to " .. self.KeyCode.Name)
+
 	if self.Main then
 		local Bindable = self.Bindable
 		if Bindable then
@@ -206,44 +206,96 @@ function Keys:__index(v)
 end
 
 function Mouse:__index(v)
-	if type(v) == "string" then
-		local Stored = newSignal()
-		rawset(self, v, Stored)
-		if find(v, "^Double") then
-			local LastClicked = 0
-			Connect(PlayerMouse[sub(v, 7)], function()
-				local ClickedTime = tick()
-				if ClickedTime - LastClicked < 0.5 then
-					FireSignal(Stored)
-					LastClicked = 0
-				else
-					LastClicked = ClickedTime
-				end
-			end)
-		else
-			local Mickey = PlayerMouse[v]
-			if find(tostring(Mickey), "Signal") then
-				Connect(Mickey, function()
-					FireSignal(Stored)
-				end)
-			end
-		end
-		return Stored
-	else
-		return PlayerMouse[v]
-	end
-end
+	local Event = newSignal()
+	rawset(self, v, Event)
 
-local function HandleInput(InputEvent, KeyEvent)
-	local RegisteredKeys, FireSignal, Keys = RegisteredKeys, FireSignal, Keys
-	Connect(InputService[InputEvent], function(KeyName, GuiInput)
-		if not GuiInput then
-			local RegisteredKey = RegisteredKeys[KeyName.KeyCode.Name]
-			if RegisteredKey then
-				FireSignal(RegisteredKey[KeyEvent])
+	if v == "DoubleButton1Click" or v == "DoubleButton1Up" then
+		local LastClicked = 0
+		Connect(PlayerMouse.Button1Up, function()
+			local ClickedTime = tick()
+			if ClickedTime - LastClicked < 0.5 then
+				FireSignal(Event)
+				LastClicked = 0
+			else
+				LastClicked = ClickedTime
 			end
+		end)
+	elseif v == "DoubleButton2Click" or v == "DoubleButton2Up" then
+		local LastClicked = 0
+		Connect(PlayerMouse.Button2Up, function()
+			local ClickedTime = tick()
+			if ClickedTime - LastClicked < 0.5 then
+				FireSignal(Event)
+				LastClicked = 0
+			else
+				LastClicked = ClickedTime
+			end
+		end)
+	--[[ No use yet
+	elseif v == "HoldButton1" or v == "HoldButton1Down" then
+		local ConnectSignal = Event.Connect
+		Event.Connect = function(a, b, c)
+			ConnectSignal(a, b)
+			local Duration = c or 1
+			local Held = -1
+			local MainConnection
+
+			local function Down()
+				local HeldSave = Held + 1
+				Held = HeldSave
+
+				FireSignal(Event)
+				while wait(Duration) and Held == HeldSave do
+					FireSignal(Event)
+				end
+			end
+
+			local function Up()
+				Held = Held + 1
+				Disconnect(MainConnection)
+				MainConnection = Connect(PlayerMouse.Button1Down, Down)
+			end
+
+			MainConnection = Connect(PlayerMouse.Button1Down, Down)
+			Connect(PlayerMouse.Button1Up, Up)
 		end
-	end)
+	elseif v == "HoldButton2" or v == "HoldButton2Down" then
+		local ConnectSignal = Event.Connect
+		Event.Connect = function(a, b, c)
+			ConnectSignal(a, b)
+			local Duration = c or 1
+			local Held = -1
+			local MainConnection
+
+			local function Down()
+				local HeldSave = Held + 1
+				Held = HeldSave
+
+				FireSignal(Event)
+				while wait(Duration) and Held == HeldSave do
+					FireSignal(Event)
+				end
+			end
+
+			local function Up()
+				Held = Held + 1
+				Disconnect(MainConnection)
+				MainConnection = Connect(PlayerMouse.Button2Down, Down)
+			end
+
+			MainConnection = Connect(PlayerMouse.Button2Down, Down)
+			Connect(PlayerMouse.Button2Up, Up)
+		end]]
+	else
+		local Mickey = PlayerMouse[v]
+		if typeof(Mickey) == "RBXScriptSignal" then
+			Connect(Mickey, function()
+				FireSignal(Event)
+			end)
+		end
+		return Mickey
+	end
+	return Event
 end
 
 local Enabled = false
@@ -252,6 +304,12 @@ local WelcomeBack = newSignal()
 local PlayerGuiBackup = Player:FindFirstChild("PlayerGuiBackup")
 local NameDisplayDistance = Player.NameDisplayDistance
 local HealthDisplayDistance = Player.HealthDisplayDistance
+
+if not PlayerGuiBackup then
+	PlayerGuiBackup = newInstance("Folder", Player)
+	PlayerGuiBackup.Name = "GuiBackup"
+end
+
 local Input = {
 	AbsentThreshold = 14;
 	CreateEvent = newSignal; -- Create a new event signal
@@ -261,22 +319,48 @@ local Input = {
 	Mouse = setmetatable(Mouse, Mouse);
 }
 
-if not PlayerGuiBackup then
-	PlayerGuiBackup = newInstance("Folder", Player)
-	PlayerGuiBackup.Name = "GuiBackup"
+function Input:__index(i)
+	local Variable = InputService[i]
+	if type(Variable) == "function" then
+		local func = Variable
+		function Variable(...) -- We need to wrap functions to mimic ":" syntax
+			return func(InputService, select(2, ...))
+		end
+		rawset(self, i, Variable)
+	end
+	return Variable
 end
 
-local function WindowFocusReleased()
+-- Connections
+Connect(InputService.InputBegan, function(KeyName, GuiInput)
+	if not GuiInput then
+		local RegisteredKey = RegisteredKeys[KeyName.KeyCode.Name]
+		if RegisteredKey then
+			FireSignal(RegisteredKey.KeyDown)
+		end
+	end
+end)
+
+Connect(InputService.InputEnded, function(KeyName, GuiInput)
+	if not GuiInput then
+		local RegisteredKey = RegisteredKeys[KeyName.KeyCode.Name]
+		if RegisteredKey then
+			FireSignal(RegisteredKey.KeyUp)
+		end
+	end
+end)
+
+Connect(InputService.WindowFocusReleased, function()
 	TimeAbsent = time()
-end
+end)
 
-local function WindowFocused()
+Connect(InputService.WindowFocused, function()
 	if time() - TimeAbsent > Input.AbsentThreshold then
 		FireSignal(WelcomeBack, TimeAbsent)
 	end
-end
+end)
 
-local function HideGui()
+ConnectSignal(Keys.Underscore.KeyDown, function()
 	if not Enabled then
 		Enabled = true
 		InputService.MouseIconEnabled = false
@@ -301,24 +385,6 @@ local function HideGui()
 			Guis[a].Parent = PlayerGuiBackup
 		end
 	end
-end
-
-function Input:__index(i)
-	local Variable = InputService[i]
-	if type(Variable) == "function" then
-		local func = Variable
-		function Variable(...) -- We need to wrap functions to mimic ":" syntax
-			return func(InputService, select(2, ...))
-		end
-		rawset(self, i, Variable)
-	end
-	return Variable
-end
-
-HandleInput("InputBegan", "KeyDown")
-HandleInput("InputEnded", "KeyUp")
-Connect(InputService.WindowFocusReleased, WindowFocusReleased)
-Connect(InputService.WindowFocused, WindowFocused)
-ConnectSignal(Keys.Underscore.KeyDown, HideGui)
+end)
 
 return setmetatable(Input, Input)
